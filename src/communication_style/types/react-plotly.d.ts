95e925295f53e5ed1ff8692053efd6bf2c7c07fc
vyeah321 <vyeah321@gmail.com>
2026-01-17 23:44:12 +0900

    ビルド修正: react-plotly の型定義に style を追加

--- src/communication_style/types/react-plotly.d.ts
+++ src/communication_style/types/react-plotly.d.ts
@@ -1,11 +1,13 @@
 declare module 'react-plotly.js' {
-  import { Component } from 'react';
+  import { Component, CSSProperties } from 'react';
   import { PlotParams } from 'plotly.js';
 
   export interface PlotProps extends Partial<PlotParams> {
     data: Partial<PlotParams['data']>;
     layout?: Partial<PlotParams['layout']>;
     config?: Partial<PlotParams['config']>;
+    // style prop is commonly used when embedding Plotly in React components
+    style?: CSSProperties;
   }
 
   export default class Plot extends Component<PlotProps> {}
